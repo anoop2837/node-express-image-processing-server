ff69ee195d78b88e149616a42f3460bd7d41fc7d
Anoop Narayanan <anoopvn@anoops-mbp.lan>
2020-04-18 00:39:13 -0700

    Completed module 2

--- api/app.js
+++ api/app.js
@@ -3,11 +3,12 @@ const bodyparser = require ('body-parser');
 const path = require('path');
 
 const app = express();
-const router = require ('./src/router');
+const router = require('./src/router');
 
 const pathToindex = path.join(__dirname, '../client/index.html');
-app.use(bodyparser.urlencoded({extended: true}));
 app.use('/', router);
+app.use(bodyparser.urlencoded({extended: true}));
+
 app.use(express.static(path.resolve(__dirname,'uploads')));
 app.use ('/*', (req, res) => {
     res.sendfile(pathToindex);
